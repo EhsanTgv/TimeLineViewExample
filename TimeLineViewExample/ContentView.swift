ed9406ca6a998a23aeb79c8d745d5aa57bc7520e
EhsanTgv <EhsanTgv@gmail.com>
2022-08-08 10:17:37 +0300

    Initial Commit

---
+++ TimeLineViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeLineViewExample
+//
+//  Created by Ehsan Taghavi on 8.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeLineViewExample/TimeLineViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLineViewExampleApp.swift
+//  TimeLineViewExample
+//
+//  Created by Ehsan Taghavi on 8.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeLineViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
