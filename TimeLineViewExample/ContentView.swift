8724f52752c076bb6e2bd592fdb87d1b59fc4389
EhsanTgv <EhsanTgv@gmail.com>
2022-08-08 10:32:37 +0300

    Add TimeLiveView example

--- TimeLineViewExample/ContentView.swift
+++ TimeLineViewExample/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            TimelineView(.animation) { context in
+                let date = context.date
+                let seconds = Calendar.current.component(.second, from: date)
+                
+                Circle()
+                    .trim(from: 0, to: Double(seconds)/60.0)
+                    .stroke(Color.blue, lineWidth: 10)
+            }
+        }.padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
